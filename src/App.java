00f68f978fc29a9ee522283fb9a3f40bcf1a1ba4
Tomas <no>
2025-04-19 12:17:39 -0300

    Finshed version with comments

--- src/App.java
+++ src/App.java
@@ -2,7 +2,8 @@ public class App {
 
     private Deck deck = new Deck();
     private Printer printer = new Printer();
-
+/** This method starts the program running getSetFromFile first then the shuffle then finally showing the set. 
+ * This code just ensures that the program starts running and in the right order*/
     public void start() {
 
         getSetFromFile();
@@ -12,9 +13,11 @@ public class App {
     }
 
    
-
+/** The getSetFromFile method gives the user a option between three sets. 
+ * It then takes the first line and the next line from the file to make a flashcard with a word and its definintion.
+ * It reads the files line by line to do so until the entire file has been read and all of the flashcards are added to the deck*/
     public void getSetFromFile() {
-       printer.output ("write CS.txt for computer science Flashcards or PE.txt for Sports Flashcards");
+       printer.output ("write CS.txt for Computer Science Flashcards, Econ.txt for Economics Flashcards, or BM.txt for Buisness Flashcards");
        String filename = printer.input();
         try {
             printer.openFile(filename);
@@ -32,7 +35,12 @@ public class App {
         }
         printer.closeFile();
     }
-
+/**
+ * This method displays the deck of flashcards card by card
+ * First it shows the word from the flash card putting it into the record.txt file to track the which words have been studied
+ * The program waits for user input to simulate flipping the flashcard to then show the definition of the word shown
+ * The program then wait for another input to show the next word and the process is repeated
+ */
     public void showSet() {
         try {
             printer.openFile("record.txt");
@@ -49,7 +57,11 @@ public class App {
     }
     }
 
-
+/**
+ * Starts the start method 
+ * @param args
+ * @throws Exception
+ */
     public static void main(String[] args) throws Exception {
         new App().start();
     }
--- src/Printer.java
+++ src/Printer.java
@@ -9,11 +9,19 @@ public class Printer {
     private File file;
     private FileWriter fileWriter;
 
+    /**
+     * This method opens the file with the give filename
+     * it throws an exception of the file can not be opened or found
+     * @param filename
+     * @throws Exception
+     */
     public void openFile(String filename) throws Exception {
         file = new File(filename);
         fileInput = new Scanner(file);
     }
-
+/**
+ * This method closes any open file that is being read or written in
+ */
     public void closeFile() {
         fileInput.close();
         file = null;
@@ -24,7 +32,12 @@ public class Printer {
             return;
         }
     }
-
+/**
+ * This method adds a line of text to the currently open file
+ * Using filewriter it writes in the given string followed by a new line
+ * filewriter is then closed and if there is a exception it is caught and ignored
+ * @param s
+ */
     public void fileOutput(String s) {
         try {
             fileWriter = new FileWriter(file,true);
@@ -37,7 +50,9 @@ public class Printer {
         }
 
     }
-
+/** 
+ * This method checks whether or not the file has a next line to read
+ */
     public boolean fileHasNextLine() {
         if (fileInput == null) {
             return false;
@@ -45,7 +60,11 @@ public class Printer {
             return fileInput.hasNextLine();
         }
     }
-
+/**
+ * This method gets the next line from the opened file
+ * it uses the filehasnextline method to check if there is a next line to get
+ * @return
+ */
     public String getNextLine() {
         if (fileHasNextLine()) {
             return fileInput.nextLine();
@@ -53,11 +72,17 @@ public class Printer {
             return "";
         }
     }
-
+/**
+ * prints the given string using System.out.println
+ * @param s
+ */
     public void output(String s) {
         System.out.println(s);
     }
-
+/**
+ * Returns the users input as a string 
+ * @return
+ */
     public String input() {
         return in.nextLine();
     }
